05e67ff5777fb95a52e6159dfc754574d05c2c3f
tva2000hn <vietanhfat@gmail.com>
2021-02-02 11:36:13 +0700

    KNN model with n_neighbors = 4

---
+++ model/knn_model.py
@@ -0,0 +1,20 @@
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from transformer.feature_transformer import FeatureTransformer
+from sklearn.neighbors import KNeighborsClassifier
+
+class KNNModel(object):
+    def __init__(self):
+        self.clf = self._init_pipeline()
+
+    @staticmethod
+    def _init_pipeline():
+        pipe_line = Pipeline([
+            ("transformer", FeatureTransformer()),
+            ("vect", CountVectorizer(stop_words= frozenset(open('filtration/vietnamese-stopwords-dash.txt', encoding="utf8").readlines()))),
+            ("tfidf", TfidfTransformer()),
+            ("clf", KNeighborsClassifier(n_neighbors=4))
+        ])
+        return pipe_line
